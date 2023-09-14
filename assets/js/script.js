a8c49025618123879af042bf89fec86b3e9b79e9
Fabiosilva112 <fabiodasilvasalustiano@gmail.com>
2023-09-13 21:13:18 -0300

    atualizações

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,26 @@ function menuShow() {
         document.querySelector('.icon').src="/assets/img/close_white_36dp.svg";
     }
 }
+
+
+let ingresso = document.querySelector(".ingresso");
+let setor = document.querySelector(".setor");
+let data = document.querySelector(".data");
+let local = document.querySelector(".local");
+
+let ingreNome = document.querySelector(".ingre-name");
+let ingreEmail = document.querySelector(".ingre-email");
+let ingreSetor = document.querySelector(".ingre-setor");
+let ingreData = document.querySelector(".ingre-data");
+let ingreLocal = document.querySelector(".ingre-local");
+
+
+
+
+function enviarIngresso() {
+    let titulo = document.querySelector(".tiketAdhered-title")   
+    let nome = document.querySelector(".nome");
+    nome.innerHTML = "Fabio Silva"
+    titulo.innerHTML = "Uhul, agora sim! Seu ingresso está aqui, apresente na entrada do evento e divirta-se!"
+    
+}
